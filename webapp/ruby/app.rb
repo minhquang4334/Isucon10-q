98e81219c0135acedf33570aede9010680160fb4
henchiyb <ducnhanyb@gmail.com>
2021-07-15 21:51:35 +0900

    Add encode

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -240,9 +240,9 @@ class App < Sinatra::Base
       halt 400
     end
 
+    data = params[:chairs][:tempfile].force_encoding(Encoding::UTF_8)
     session.with_transaction(write_concern: {w: :majority}, read: {mode: :primary}) do
-      CSV.parse(params[:chairs][:tempfile].read, skip_blanks: true) do |row|
-        row = row.map(&:to_s)
+      CSV.parse(data.read, skip_blanks: true) do |row|
         object = {
           :id => row[0].to_s,
           :name => row[1].to_s,
@@ -459,9 +459,9 @@ class App < Sinatra::Base
       halt 400
     end
 
+    data = params[:estates][:tempfile].force_encoding(Encoding::UTF_8)
     session.with_transaction(write_concern: {w: :majority}, read: {mode: :primary}) do
-      CSV.parse(params[:estates][:tempfile].read, skip_blanks: true) do |row|
-        row = row.map(&:to_s)
+      CSV.parse(data.read, skip_blanks: true) do |row|
         object = {
           :id => row[0].to_s,
           :name => row[1].to_s,
